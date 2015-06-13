c736fae3901638694f6d2bf6251cf7b835798acc
liuzhongshu <liuzhongshu@gmail.com>
2015-06-13 14:04:56 +0800

    update rest api

--- src/main/java/com/liuzhongshu/lab/DbManager.java
+++ src/main/java/com/liuzhongshu/lab/DbManager.java
@@ -3,11 +3,15 @@ package com.liuzhongshu.lab;
 import io.dropwizard.lifecycle.Managed;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DbManager implements Managed {
@@ -26,25 +30,88 @@ public class DbManager implements Managed {
 
 	@Override
 	public void stop() throws Exception {
-		objectMapper.writeValue(new File("db.json"), memDb);		
 	}
 
-	public List<Map> list(String model) {
+	public List<Map> list(String model) throws Exception {
 		Object entities = memDb.get(model);
-		if ( entities instanceof List) {
-			return (List<Map>)entities;
+		if(!(entities instanceof List))
+			throw new Exception("model not found");
+
+		return (List<Map>)entities;
+	}
+
+	public Map<String,Object> getById(String model, long id) throws Exception {
+		Object entities = memDb.get(model);
+		if(!(entities instanceof List))
+			throw new Exception("model not found");
+		
+		for (Map entity : (List<Map>)entities) {
+			if (entity.get("id") != null && id == Long.parseLong(entity.get("id").toString()))
+				return entity;
 		}
-		return null;
+		
+		throw new Exception("entity not found");
+	}
+	
+	public void save() throws Exception {
+		objectMapper.writeValue(new File("db.json"), memDb);
 	}
 
-	public Map<String,Object> getById(String model, long id) {
+	public void delteById(String model, long id) throws Exception {
 		Object entities = memDb.get(model);
-		if ( entities instanceof List) {
-			for (Map entity : (List<Map>)entities) {
-				if (entity.get("id") != null && id == Long.parseLong(entity.get("id").toString()))
-					return entity;
+		if(!(entities instanceof List))
+			throw new Exception("model not found");
+		
+		Iterator<Map> iter = ((List) entities).iterator();
+		while (iter.hasNext()) {
+			Map entity = iter.next();
+			if (entity.get("id") != null && id == Long.parseLong(entity.get("id").toString())) {
+				iter.remove();
+				save();
+				return;
 			}
 		}
-		return null;
+		
+		throw new Exception("entity not found");
+	}
+
+	public void create(String model, Map<String, Object> entity) throws Exception {
+		Object entities = memDb.get(model);
+		if(!(entities instanceof List))
+			throw new Exception("model not found");
+		
+		try {
+			long id = Long.parseLong(entity.get("id").toString());
+		}catch(Exception e) {
+			throw new Exception("entity should have id");
+		}
+		
+		((List)entities).add(entity);
+		save();
+	}
+
+	public void update(String model, Map<String, Object> entity) throws Exception {
+		Object entities = memDb.get(model);
+		if(!(entities instanceof List))
+			throw new Exception("model not found");
+		
+		long id = 0;
+		try {
+			id = Long.parseLong(entity.get("id").toString());
+		}catch(Exception e) {
+			throw new Exception("entity should have id");
+		}
+		
+		Iterator<Map> iter = ((List) entities).iterator();
+		while (iter.hasNext()) {
+			Map item = iter.next();
+			if (entity.get("id") != null && id == Long.parseLong(item.get("id").toString())) {
+				iter.remove();
+				((List) entities).add(entity);
+				save();
+				return;
+			}
+		}		
+		throw new Exception("entity not found");
 	}
 }
--- src/main/java/com/liuzhongshu/lab/RestResource.java
+++ src/main/java/com/liuzhongshu/lab/RestResource.java
@@ -3,9 +3,7 @@ package com.liuzhongshu.lab;
 import java.util.List;
 import java.util.Map;
 
-import io.dropwizard.setup.Environment;
-
-import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/{model}")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,32 +25,35 @@ public class RestResource {
 	}
 	
 	@GET
-    public List<Map> list(@PathParam("model") String model) {
-		
+    public List<Map> list(@PathParam("model") String model) throws Exception {
         return dbManager.list(model);
     }
 	
 	@GET
     @Path("{id:[0-9]+?}")
-    public Map<String,Object> getById(@PathParam("model") String model, @PathParam("id") long id) {
+    public Map<String,Object> getById(@PathParam("model") String model, @PathParam("id") long id) throws Exception {
         return dbManager.getById(model, id);
     }
 	
 	@POST
-    public String create(@PathParam("model") String model) {
-        return model;
+	@Consumes(MediaType.APPLICATION_JSON)
+    public JsonResult create(@PathParam("model") String model, Map<String, Object> entity) throws Exception {
+		dbManager.create(model, entity);
+        return new JsonResult();
     }
 	
 	@PUT
     @Path("{id:[0-9]+?}")
-    public String modify(@PathParam("model") String model) {
-        return model;
+    public JsonResult update(@PathParam("model") String model, Map<String, Object> entity) throws Exception {
+		dbManager.update(model, entity);
+		return new JsonResult();
     }
 	
 	@DELETE
     @Path("{id:[0-9]+?}")
-    public String delete(@PathParam("model") String model) {
-        return model;
+    public JsonResult delete(@PathParam("model") String model, @PathParam("id") long id) throws Exception {
+		dbManager.delteById(model, id);
+		return new JsonResult();
     }
 	
 }
